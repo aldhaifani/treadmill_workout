e5ee92f223f57b5154f1dc6f7723e7b3fb100815
Tareq <tareqaldhaifani@gmail.com>
2023-11-24 16:26:25 +0400

    extract data from xlsx file

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template
+import openpyxl
 
 
 app = Flask(__name__)
 app.secret_key = "HdraYnfHEiTKsR3kFJbfnMdLAfUYGiew"
+
+wb = openpyxl.load_workbook("./static/data_sheets/treadmill_workout.xlsx")
+ws = wb.active
+
+data_obj = [
+    {
+        "label": ws.cell(row=row, column=1).value,
+        "time": ws.cell(row=row, column=2).value,
+        "incline": ws.cell(row=row, column=3).value,
+        "speed": ws.cell(row=row, column=4).value,
+    }
+    for row in range(2, ws.max_row + 1)
+]
 
 
 @app.route("/")
